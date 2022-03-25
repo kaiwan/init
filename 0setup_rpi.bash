5382e3f77f6194a25bf7894a371444b053dd2400
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2022-03-25 08:08:45 +0530

    for core_pattern, first set fs.suid_dumpable to 1 - debug mode

--- 0setup_rpi.bash
+++ 0setup_rpi.bash
@@ -77,6 +77,9 @@ alias sd='sudo /bin/bash'
   echo "8 4 1 4" > /proc/sys/kernel/printk
   # better core pattern
   [ $(id -u) -eq 0 ] && {
+   # first set fs.suid_dumpable to 1 - debug mode
+   # ref: https://www.kernel.org/doc/html/latest/admin-guide/sysctl/fs.html#suid-dumpable
+   echo 1 > /proc/sys/fs/suid_dumpable
    echo "corefile:host=%h:gPID=%P:gTID=%I:ruid=%u:sig=%s:exe=%E" > /proc/sys/kernel/core_pattern
   }
 }
