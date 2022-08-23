a8397d2130720e3043254b4f365a9f1497fc5993
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2022-08-23 08:53:30 +0530

    prettify; add -y to apt install

--- apt_install_common_stuff.sh
+++ apt_install_common_stuff.sh
@@ -15,5 +15,12 @@ sudo apt update
 sudo apt install gcc make perl git
 
 # everything
-sudo apt install fakeroot build-essential tar ncurses-dev tar xz-utils libssl-dev bc stress python3-distutils libelf-dev linux-headers-$(uname -r) bison flex libncurses5-dev util-linux net-tools linux-tools-$(uname -r) exuberant-ctags cscope sysfsutils gnome-system-monitor curl perf-tools-unstable gnuplot rt-tests indent tree pstree smem libnuma-dev numactl hwloc bpfcc-tools sparse flawfinder cppcheck tuna hexdump openjdk-14-jre trace-cmd virt-what
+sudo apt install fakeroot build-essential tar ncurses-dev tar xz-utils \
+		libssl-dev bc stress python3-distutils libelf-dev \
+		linux-headers-$(uname -r) bison flex libncurses5-dev util-linux \
+		net-tools linux-tools-$(uname -r) exuberant-ctags cscope sysfsutils \
+		gnome-system-monitor curl perf-tools-unstable gnuplot rt-tests indent \
+		tree pstree smem libnuma-dev numactl hwloc bpfcc-tools sparse \
+		flawfinder cppcheck tuna hexdump openjdk-14-jre trace-cmd virt-what file \
+		-y
 
